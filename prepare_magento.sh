48d25c8bb1f6f11d9c210648dc2f5b81e6683f45
Flyingmana <flyingmana@googlemail.com>
2018-11-17 21:26:50 +0100

    execute a magento install and prepare running a webserver

--- prepare_magento.sh
+++ prepare_magento.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 git clone https://github.com/OpenMage/magento-lts.git magento
-cd magento
+./vendor/bin/n98-magerun install --noDownload \
+    --dbHost="localhost" --dbUser="travis" \
+    --dbPass="" --dbName="magentodb" --installSampleData=yes \
+    --useDefaultConfigParams=yes \
+    --magentoVersionByName="magento-mirror-1.9.3.10" \
+    --installationFolder="magento" --baseUrl="http://magento.localdomain/"
+
+php -t magento/ -S 127.0.0.5:8080 php-dev-router.php&
+
--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 ./vendor/bin/phpunit
+
+curl 127.0.0.5:8080
