5cf849392871325eb9e77fe6c276e7169d203c1a
Flyingmana <flyingmana@googlemail.com>
2018-11-18 11:16:57 +0100

    add fast-simple-import to install

--- .travis/install_magento.php
+++ .travis/install_magento.php
@@ -7,7 +7,29 @@ passthru('./vendor/bin/n98-magerun install --noDownload \
     --dbPass="" --dbName="magentodb" --installSampleData=yes \
     --useDefaultConfigParams=yes \
     --magentoVersionByName="magento-mirror-1.9.3.10" \
-    --installationFolder="magento" --baseUrl="http://magento.localdomain/"
+    --installationFolder="magento" --baseUrl="http://127.0.0.5:8080/"
 ');
 
+$composerJson = <<<JSON
+{
+    "require":{
+        "magento-hackathon/magento-composer-installer": "*",
+        "avstudnitz/fast-simple-import":"*"
+    },
+    "extra":{
+        "magento-root-dir": "./"
+    }
+}
+JSON;
+
+chdir(__DIR__ . '/../magento');
+
+file_put_contents('composer.json', $composerJson);
+passthru('composer install');
+
+//passthru('composer install magento-hackathon/magento-composer-installer');
+//passthru('composer install avstudnitz/fast-simple-import');
+
+
+chdir(__DIR__ . '/../');
 
