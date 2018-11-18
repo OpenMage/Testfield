cc504df8050462571f475680ea44d7cdcba7c842
Flyingmana <flyingmana@googlemail.com>
2018-11-18 13:44:01 +0100

    let magento handle 404s

--- php-dev-router.php
+++ php-dev-router.php
@@ -9,7 +9,9 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif|ico)$/', $_SERVER["REQUEST_URI"])) {
 } elseif (preg_match('/^\/index\.php(\/)?/', $_SERVER["REQUEST_URI"])) {
     return false;    // serve the requested resource as-is.
 } else {
-    header('HTTP/1.0 404 Not Found');
-    echo "<p>Welcome to PHP</p>";
-    var_dump($_SERVER["REQUEST_URI"]);
+    chdir(__DIR__.'/magento/');
+    include(__DIR__.'/magento/index.php');
+//    header('HTTP/1.0 404 Not Found');
+//    echo "<p>Welcome to PHP</p>";
+//    var_dump($_SERVER["REQUEST_URI"]);
 }
