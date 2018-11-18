f29509baac6e17d12dd60ef0e95ca8cec1f70778
Flyingmana <flyingmana@googlemail.com>
2018-11-18 10:37:31 +0100

    add some docker-compose helping files

---
+++ .docker/install_magento.sh
@@ -0,0 +1,2 @@
+
+./vendor/bin/n98-magerun install --noDownload --dbHost="127.0.0.1" --dbUser="magentodb" --dbPass="magentodb" --dbName="magentodb" --installSampleData=yes --useDefaultConfigParams=yes --magentoVersionByName="magento-mirror-1.9.3.10" --installationFolder="magento" --baseUrl="http://127.0.0.5:8080/
