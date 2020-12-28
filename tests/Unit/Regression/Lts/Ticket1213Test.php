7ce6773283f00a73b0ca1431667b034fb94a55b0
Flyingmana <flyingmana@googlemail.com>
2020-12-28 18:27:49 +0100

    add Regression Test Ticket1213Test

---
+++ tests/Unit/Regression/Lts/Ticket1213Test.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace OpenMage\Testfield\Unit\Regression\Lts;
+
+use PHPUnit\Framework\TestCase;
+
+class Ticket1213Test extends TestCase
+{
+
+    /**
+     *
+     * triggered before fixing this error in newer PHP Versions:
+     * "strpos(): Non-string needles will be interpreted as strings in the future. Use an explicit chr() call to preserve the current behavior"
+     *
+     *
+     */
+    public function testDefaultState()
+    {
+        $object = new \Mage_Eav_Model_Entity_Increment_Numeric();
+
+        $this->assertEquals(null, $object->getPrefix());
+        $this->assertEquals(null, $object->getLastId());
+
+        $this->assertEquals('00000001', $object->getNextId());
+
+    }
+}
