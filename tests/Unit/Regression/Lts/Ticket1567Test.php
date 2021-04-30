c1b59e756e6ec1c88e9826335899a3152840f09e
Flyingmana <flyingmana@googlemail.com>
2021-04-30 10:35:42 +0200

    skip tests for older OpenMage versions

--- tests/Unit/Regression/Lts/Ticket1567Test.php
+++ tests/Unit/Regression/Lts/Ticket1567Test.php
@@ -30,6 +30,9 @@ class Ticket1567Test extends TestCase
 
     public function testMagicSetWithMiddleDoubleUpperCase()
     {
+        if (Mage::getOpenMageVersionInfo()['major'] === 19) {
+            $this->markTestSkipped("only solved from 20.x on");
+        }
         $object = new \Varien_Object();
 
         $object->setMyATestingValue('a_normal_string');
@@ -40,6 +43,9 @@ class Ticket1567Test extends TestCase
 
     public function testSetDataWithMiddleDoubleUpperCase()
     {
+        if (Mage::getOpenMageVersionInfo()['major'] === 19) {
+            $this->markTestSkipped("only solved from 20.x on");
+        }
         $object = new \Varien_Object();
 
         $object->setData('my_a_testing_value', 'a_normal_string');
