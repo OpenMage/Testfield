0bb64255a2a90f105d8e21b709c9b24df0ef2145
Flyingmana <flyingmana@googlemail.com>
2024-02-03 21:21:30 +0100

    run phpunit with debug instead of -vvv

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./vendor/bin/phpunit -c ./tests/Unit/phpunit.xml.dist --testsuite=Unit -vvv --log-junit output/junit-unit-report.xml
+./vendor/bin/phpunit -c ./tests/Unit/phpunit.xml.dist --testsuite=Unit --debug --log-junit output/junit-unit-report.xml
